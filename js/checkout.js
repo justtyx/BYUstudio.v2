d029c2d69fbd1938badd3d712bc07105aacb5857
Rikke Blom <rikk957v@stud.kea.dk>
2021-06-02 18:11:01 +0200

    more styling

--- js/checkout.js
+++ js/checkout.js
@@ -1,27 +1,7 @@
-const mobilesummary = document.querySelector(".ordersummarymobile");
-mobilesummary.addEventListener("click", toggleSummary);
 const creditcard = document.querySelector("#creditcard");
 const paypal = document.querySelector("#paypal");
 creditcard.addEventListener("click", togglePaymentForm);
 paypal.addEventListener("click", togglePaymentForm);
-
-let val = 1;
-
-function toggleSummary() {
-  if (val == 1) {
-    document.querySelector(".summaryflex > img").classList.add("rotatearrow");
-    document.querySelector(".summaryphone").classList.remove("hidden");
-    val = val + 1;
-    console.log("open");
-  } else if (val == 2) {
-    document
-      .querySelector(".summaryflex > img")
-      .classList.remove("rotatearrow");
-    document.querySelector(".summaryphone").classList.add("hidden");
-    val = val - 1;
-    console.log("close");
-  }
-}
 
 // PAYMENT OPTION
 function togglePaymentForm() {
---
+++ js/ordersummary.js
@@ -0,0 +1,19 @@
+const mobilesummary = document.querySelector(".ordersummarymobile");
+mobilesummary.addEventListener("click", toggleSummary);
+let val = 1;
+
+function toggleSummary() {
+  if (val == 1) {
+    document.querySelector(".summaryflex > img").classList.add("rotatearrow");
+    document.querySelector(".summaryphone").classList.remove("hidden");
+    val = val + 1;
+    console.log("open");
+  } else if (val == 2) {
+    document
+      .querySelector(".summaryflex > img")
+      .classList.remove("rotatearrow");
+    document.querySelector(".summaryphone").classList.add("hidden");
+    val = val - 1;
+    console.log("close");
+  }
+}
