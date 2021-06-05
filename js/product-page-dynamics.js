26918243e63f799183cc1746d718885c289a1dae
Justina Jemeljanovaite <just1528@stud.kea.dk>
2021-06-06 00:54:15 +0300

    made a primitve version of product page

---
+++ js/product-page-dynamics.js
@@ -0,0 +1,23 @@
+const fetchUrl = 'https://kea21-7e1e.restdb.io/rest/byu-products?q={"name": "Standard kit"}';
+//const mediaurl = 'https://kea2021-8b3d.restdb.io/media/'
+
+fetch(fetchUrl, {
+    "method": "GET",
+    "headers": {
+      "x-apikey": "602f9e445ad3610fb5bb63d5"
+    }
+  })
+  .then((res) => res.json())
+  .then(response => {
+    showProduct(response);
+  })
+  .catch(err => {
+    console.error(err);
+});
+
+function showProduct(product) {
+console.log(product);
+// document.querySelector('.product-image').src = product[0].picture;
+document.querySelector('.top-details>h1').textContent = product[0].name;
+document.querySelector('.top-details>h2>span').textContent = product[0].price;
+}
